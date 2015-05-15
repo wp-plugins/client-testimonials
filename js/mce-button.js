3466ba3dd5c603dfcefa355e5c9c6ffaf4208b09
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 05:35:01 +0000

    Some Changes
    git-svn-id: https://plugins.svn.wordpress.org/client-testimonials/trunk@1160928 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/mce-button.js
+++ js/mce-button.js
@@ -4,62 +4,107 @@
 
             title : 'Add Client Testimonials',
 			image : url + '/icon.png',
-			onclick: function() {
-				editor.windowManager.open( {
-					title: 'Insert Client Testimonials Shortcode',
-					body: 
-					[
-						{
-							type: 'textbox',
-							name: 'items_desktop',
-							label: 'Items Desktop',
-							value: '1'
-						},
-						{
-							type: 'textbox',
-							name: 'items_tablet',
-							label: 'Items Tablet',
-							value: '1'
-						},
-						{
-							type: 'textbox',
-							name: 'items_tablet_small',
-							label: 'Items Tablet Small',
-							value: '1'
-						},
-						{
-							type: 'textbox',
-							name: 'items_mobile',
-							label: 'Items Mobile',
-							value: '1'
-						},
-						{
-							type: 'textbox',
-							name: 'posts_per_page',
-							label: 'Total numbers of Testimonials to show',
-							value: '-1'
-						},
-						{
-							type: 'listbox',
-							name: 'orderby',
-							label: 'Order By',
-								'values': 
-								[
-									{text: 'None', value: 'none'},
-									{text: 'ID', value: 'ID'},
-									{text: 'Date', value: 'date'},
-									{text: 'Modified', value: 'modified'},
-									{text: 'Rand', value: 'rand'}
-								]
-						}
-					],
-					onsubmit: function( e ) {
-						editor.insertContent( '[client-testimonials items_desktop="' + e.data.items_desktop + '" items_tablet="' + e.data.items_tablet + '" items_tablet_small="' + e.data.items_tablet_small + '" items_mobile="' + e.data.items_mobile + '" posts_per_page="' + e.data.posts_per_page + '" orderby="' + e.data.orderby + '"]');
-						}
+			type: 'menubutton',
+			menu: [
+				{
+					text: 'Client Testimonials Slide',
+					onclick: function() {
+						editor.windowManager.open( {
+							title: 'Insert Client Testimonials Shortcode',
+							body: 
+							[
+								{
+									type: 'textbox',
+									name: 'items_desktop',
+									label: 'Items Desktop',
+									value: '1'
+								},
+								{
+									type: 'textbox',
+									name: 'items_tablet',
+									label: 'Items Tablet',
+									value: '1'
+								},
+								{
+									type: 'textbox',
+									name: 'items_tablet_small',
+									label: 'Items Tablet Small',
+									value: '1'
+								},
+								{
+									type: 'textbox',
+									name: 'items_mobile',
+									label: 'Items Mobile',
+									value: '1'
+								},
+								{
+									type: 'textbox',
+									name: 'posts_per_page',
+									label: 'Total numbers of Testimonials to show',
+									value: '-1'
+								},
+								{
+									type: 'listbox',
+									name: 'orderby',
+									label: 'Order By',
+										'values': 
+										[
+											{text: 'None', value: 'none'},
+											{text: 'ID', value: 'ID'},
+											{text: 'Date', value: 'date'},
+											{text: 'Modified', value: 'modified'},
+											{text: 'Rand', value: 'rand'}
+										]
+								}
+							],
+							onsubmit: function( e ) {
+								editor.insertContent( '[client-testimonials items_desktop="' + e.data.items_desktop + '" items_tablet="' + e.data.items_tablet + '" items_tablet_small="' + e.data.items_tablet_small + '" items_mobile="' + e.data.items_mobile + '" posts_per_page="' + e.data.posts_per_page + '" orderby="' + e.data.orderby + '"]');
+								}
+							}
+						);
 					}
-				);
-			}
-
+				}, // End Client Testimonials Slide
+				{
+					text: 'Client Testimonials NO Slide',
+					onclick: function() {
+						editor.windowManager.open( {
+							title: 'Insert Client Testimonials Shortcode',
+							body: 
+							[
+								{
+									type: 'textbox',
+									name: 'testimonial_id',
+									label: 'Testimonial ID',
+									value: ''
+								},
+								{
+									type: 'textbox',
+									name: 'posts_per_page',
+									label: 'Total numbers of Testimonials to show',
+									value: '1'
+								},
+								{
+									type: 'listbox',
+									name: 'orderby',
+									label: 'Order By',
+										'values': 
+										[
+											{text: 'None', value: 'none'},
+											{text: 'ID', value: 'ID'},
+											{text: 'Date', value: 'date'},
+											{text: 'Modified', value: 'modified'},
+											{text: 'Rand', value: 'rand'}
+										]
+								}
+							],
+							onsubmit: function( e ) {
+								editor.insertContent( '[testimonial testimonial_id="' + e.data.testimonial_id + '" posts_per_page="' + e.data.posts_per_page + '" orderby="' + e.data.orderby + '"]');
+								}
+							}
+						);
+					}
+				} // End Client Testimonials NO Slide
+			]
 		});
 	});
 })();
